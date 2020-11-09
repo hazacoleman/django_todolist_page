a96421c01e366b9e12a614198801273993e7ef43
Harry Coleman <63397607+hazacoleman@users.noreply.github.com>
2020-11-09 11:04:53 +1100

    Update settings.py

--- Django1stTut/settings.py
+++ Django1stTut/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '&puo-a^-msjl7ut9%!t5-hw)6%k$a1+ua4dp1y&j0m^5jtbvu^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
